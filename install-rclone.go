b3af943754520e4fb46287ecbdf9dfacb4300fc8
Johnson <JXJ1522@sfgmembers.com>
2021-01-15 13:36:28 -0600

    updated rclone installer

--- install-rclone.go
+++ install-rclone.go
@@ -6,7 +6,7 @@ import(
 )
 
 func main() {    
-    c := exec.Command("cmd", "/c", "copy", "rclone.exe", "rclone.new.exe")
+    c := exec.Command("cmd", "/c", "copy", "C:\\Users\\jxj1522\\source\\repos\\temp\\rclone.exe", "C:\\Users\\jxj1522\\source\\repos\\temp\\rclone.new.exe")
 
     if err := c.Run(); err != nil { 
         fmt.Println("Error: ", err)
