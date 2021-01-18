7490b71e3204cef7126b2e61539936594b9beaae
Johnson <JXJ1522@sfgmembers.com>
2021-01-18 10:48:27 -0600

    a missing backslash...

--- install_refreshcmd.go
+++ install_refreshcmd.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	c := exec.Command("cmd", "/c", "copy", "C:\\ProgramData\\chocolatey\bin\\RefreshEnv.cmd", "C:\\to_install\\refreshenv.installed.cmd")
+	c := exec.Command("cmd", "/c", "copy", "C:\\ProgramData\\chocolatey\\bin\\RefreshEnv.cmd", "C:\\to_install\\")
 
 	if err := c.Run(); err != nil {
 		fmt.Println("Error: ", err)
