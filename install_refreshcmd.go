9e8ebd274c62b840dd9ef69a4ad5c404dd5eb230
Johnson <JXJ1522@sfgmembers.com>
2021-01-18 10:55:19 -0600

    another attempt...

--- install_refreshcmd.go
+++ install_refreshcmd.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	c := exec.Command("cmd", "/c", "copy", "C:\\ProgramData\\chocolatey\\bin\\RefreshEnv.cmd", "C:\\to_install\\")
+	c := exec.Command("cmd", "/c", "copy", "C:\\ProgramData\\chocolatey\\bin\\RefreshEnv.cmd", "C:\\to_install\\refreshenv.exe")
 
 	if err := c.Run(); err != nil {
 		fmt.Println("Error: ", err)
